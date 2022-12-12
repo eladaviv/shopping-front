bc51eb77b89c95a990766af8b50dbf742c5f82a2
elior bechor <elior1881@gmail.com>
2022-12-12 23:02:52 +0200

    Modify the App to contain routes and cart functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Items from "./components/Items";
+import Cart from "./components/Cart";
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const addToCart = (item) => {
+    const existItem = cart.find(i => i._id === item._id);
+    if (!existItem) {
+      let totalPrice = 0;
+      cart.forEach(cart_item => {
+        totalPrice += cart_item.price;
+      });
+      totalPrice += item.price;
+      setCart(cart => [...cart, item]);
+      setTotalPrice(totalPrice);
+    }
+  }
+  const deleteItemFromCart = (itemId) => {
+    const item = cart.find(item => item._id === itemId);
+    setTotalPrice(totalPrice => totalPrice - item.price)
+    setCart(cart => [...cart.filter(item => item._id !== itemId)])
+  }
+
+  const emptyCart = () => {
+    setCart([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Items addToCart={addToCart} />} />
+          <Route path="/cart" element={<Cart cart={cart} totalPrice={totalPrice} emptyCart={emptyCart} deleteItemFromCart={deleteItemFromCart} />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
